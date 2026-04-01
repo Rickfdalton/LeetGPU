047b158ee4a2c97a970e277ef8d999da1ab075dd
jathavan mahendrarajah <physickness@gmail.com>
2026-04-01 17:37:39 +0530

    feat:relu

---
+++ relu.cu
@@ -0,0 +1,20 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void relu_kernel(const float* input, float* output, int N) {
+    int idx = blockDim.x* blockIdx.x + threadIdx.x;
+
+    if(idx < N ){
+        float input_val= input[idx];
+        output[idx]=(fabsf(input_val)+input_val)/2.0f; 
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    relu_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
